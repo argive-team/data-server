e73542bf5360e62d2f1ba84593c46276f92abba5
Duc T. Lam <ducl_13@icloud.com>
2017-04-27 23:05:37 -0700

    ReviewController factory class.

---
+++ www/module/Import/src/Import/Factory/Controller/ReviewControllerFactory.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/Import for the canonical source repository
+ * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Import\Factory\Controller;
+
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+use Import\Controller\ReviewController;
+
+class ReviewControllerFactory implements FactoryInterface
+{
+    public function createService(ServiceLocatorInterface $container)
+    {
+        $parentLocator = $container->getServiceLocator();
+        $config = $parentLocator->get('Config');
+        
+        return new ReviewController($config);
+    }
+}
